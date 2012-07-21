830fc590023cf79cf17e493e0152739d4ab6ece7
Takao Noguchi <tkngch@runbox.com>
2012-07-21 14:11:45 +0100

    tidied up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='feedpy',
+        version='2012.07.21',
+        author='Takao Noguchi',
+        author_email='tkngch@runbox.com',
+        license='LICENSE.txt',
+        long_description=open('README.txt').read(),
+)
